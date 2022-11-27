f12f8ca6954bbf56fd9d7a88870f00d071a297a0
sherin200 <sherinsebastian21>
2022-11-27 21:48:14 +0530

    data binding

--- src/app/view-employee/view-employee.component.ts
+++ src/app/view-employee/view-employee.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgSelectOption } from '@angular/forms';
 
 @Component({
   selector: 'app-view-employee',
@@ -6,5 +7,44 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-employee.component.css']
 })
 export class ViewEmployeeComponent {
-
+  data:any = [
+    {
+        "name":"Sherin",
+        "designation" :"Software Engineer Trainee",
+        "salary":30000,
+        "Company":"Nest"
+    
+    },
+    {
+      "name":"Susan",
+      "designation" :"Software Engineer Trainee",
+      "salary":40000,
+      "Company":"UST"
+    },
+  {
+    "name":"Joel",
+    "designation" :"Software Engineer Trainee",
+    "salary":60000,
+    "Company":"TCS"
+  },
+  {
+    "name":"Anna",
+    "designation" :"Software Engineer Trainee",
+    "salary":90000,
+    "Company":"UST"
+  },
+  {
+    "name":"Sus",
+    "designation" :"Software Engineer Trainee",
+    "salary":70000,
+    "Company":"JSU"
+  },
+  {
+    "name":"Revi",
+    "designation" :"Software Engineer Trainee",
+    "salary":40000,
+    "Company":"TCS"
+  }
+]
 }
+
